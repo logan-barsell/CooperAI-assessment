824abcd3f0156ea46258e09ee34e5bbf2eea5131
Stephen Clarkson <stephen.clarkson@getelements.com>
2024-09-13 11:19:55 -0700

    update meta tags

--- app/root.tsx
+++ app/root.tsx
@@ -6,8 +6,6 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 import "./tailwind.css";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { useState } from "react";
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
@@ -17,7 +15,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
-        <title>Cooper AI Front-end Coding Challenge</title>
+        <title>Cooper AI Coding Challenge</title>
       </head>
       <body>
         {children}
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,8 +2,11 @@ import type { MetaFunction } from "@remix-run/node";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: "Cooper AI Coding Challenge" },
+    {
+      name: "description",
+      content: "Welcome to the Cooper AI Coding Challenge!",
+    },
   ];
 };
 
